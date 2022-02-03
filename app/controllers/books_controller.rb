0347e2fc50fe2344e59a4778260a5419b693d8a8
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-03 10:04:15 +0000

    Fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,16 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end  
   end
   
   def index
+    @book = Book.new
     @books = Book.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get 'books/new'
-  
+  post 'books' => 'books#create', as: 'books'
   get 'books' => 'books#index'
-  post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
